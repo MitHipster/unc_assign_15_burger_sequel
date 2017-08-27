996a6409d3b2e1802d1ed7d80cfa5a69fe52660a
Tim Acker <tjacker31@gmail.com>
2017-08-27 18:30:26 -0400

    Button Issue
    Fixed full-width issue with 'devour it' button in card component.

--- db/rating_seeds.sql
+++ db/rating_seeds.sql
@@ -1,5 +1,5 @@
 INSERT INTO Ratings (rating_value, description) VALUES (1, "Never again!");
 INSERT INTO Ratings (rating_value, description) VALUES (2, "Not worth it.");
-INSERT INTO Ratings (rating_value, description) VALUES (3, "Okay in a pinch.");
+INSERT INTO Ratings (rating_value, description) VALUES (3, "Okay I guess.");
 INSERT INTO Ratings (rating_value, description) VALUES (4, "Really good.");
 INSERT INTO Ratings (rating_value, description) VALUES (5, "Just awesome!");
